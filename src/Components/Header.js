076b84a6ef3ffbdaae4a3775db944f1cfc0ba901
m.ilad <hosseini.milad1400@gmail.com>
2022-09-14 21:03:00 +0430

    initial startup

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,39 @@
 function Header(){
     return(
-        <header>header</header>
+        <header>
+            <div className="topBanner">
+                <img src="/topBanner.jpg" />
+            </div>
+            <div className="headerTop">
+                <div className="logoHeader">
+                    <img src="/logo.png" />
+                </div>
+                <div className="searchHeader">
+                    <div className="searchHolder">
+                        <input type="text" placeHolder="What are youe" />
+                    </div>
+                    <div className="searchButton">
+                        <span>Search</span>
+                    </div>
+                </div>
+                <div className="clientField">
+                    <div className="loginHeader">
+                        Login
+                    </div>
+                    <div className="clientHeader">
+                        <div className="clientPart">
+                            Messages
+                        </div>
+                        <div className="clientPart">
+                            Orders
+                        </div>
+                        <div className="clientPart">
+                            Cart
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 export default Header
